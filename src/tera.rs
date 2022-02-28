f8584a42db6fb102e33c0beed61266019f47ba8e
Aaron Tan <hi@aaronhktan.com>
2022-02-28 03:29:13 -0500

    Add setup + not playing screen
    - When a user is not listening to music, the "not playing" screen shows up. It shows the total number of tracks that a user has listened to and album art from the user's top albums of the week.
    - If a user did not provide user and key params in the URL, they are redirected to the setup page.

--- src/tera.rs
+++ src/tera.rs
@@ -1,4 +1,7 @@
 use std::collections::HashMap;
+use std::convert::From;
+
+use chrono::NaiveDateTime;
 
 use rocket::serde::Serialize;
 use rocket::Request;
@@ -9,8 +12,7 @@ mod lastfm;
 
 #[derive(Serialize)]
 #[serde(crate = "rocket::serde")]
-struct TemplateContext<'r> {
-    name: &'r str,
+struct NowPlayingContext<'r> {
     track: &'r str,
     artist: &'r str,
     album: &'r str,
@@ -19,15 +21,51 @@ struct TemplateContext<'r> {
     key: &'r str,
 }
 
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+struct Album {
+    name: String,
+    album_art: String,
+}
+
+impl From<&lastfm::Album> for Album {
+    // Is there a better way to implement this?
+    // I don't want to put the Serialize macro into the lastfm module,
+    // which is why I have to convert the lastfm::Album to tera::Album.
+    fn from(album: &lastfm::Album) -> Self {
+        Album {
+            name: album.name.clone(),
+            album_art: album.album_art.clone(),
+        }
+    }
+}
+
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+struct NotPlayingContext<'r> {
+    scrobble_count: &'r u32,
+    account_creation: &'r str,
+    albums: Vec<Album>,
+    user: &'r str,
+    key: &'r str,
+}
+
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+struct ApiContext<'r> {
+    user: Option<&'r str>,
+    key: Option<&'r str>,
+}
+
 #[get("/?<user>&<key>")]
 pub async fn hello(user: Option<&str>, key: Option<&str>) -> Template {
     if let (Some(user), Some(key)) = (user, key) {
         if let Some(track) = lastfm::get_current_playing(user, key).await.unwrap() {
+            // User is listening to music! Render the now playing screen.
             println!("{:?}", track);
             return Template::render(
                 "index",
-                &TemplateContext {
-                    name: user,
+                &NowPlayingContext {
                     track: &track.name,
                     artist: &track.artist,
                     album: &track.album,
@@ -36,32 +74,38 @@ pub async fn hello(user: Option<&str>, key: Option<&str>) -> Template {
                     key: &key,
                 },
             );
+        } else {
+            // User is not listening to music: show them stats for albums in the past week.
+            let user_stats = lastfm::get_user_stats(user, key);
+            let album_stats = lastfm::get_top_albums(user, key, lastfm::QueryPeriod::SevenDay, &9);
+
+            let user_stats = user_stats.await.unwrap();
+            let album_stats = album_stats.await.unwrap();
+
+            // Convert account creation epoch time to string
+            let account_creation = NaiveDateTime::from_timestamp(user_stats.registered_time, 0)
+                    .date()
+                    .format("%-d %B %Y")
+                    .to_string();
+
+            // Convert lastfm::Album to tera::Album so the objects can be serialized by Rocket for Tera
+            let albums = album_stats.iter().map(|album| Album::from(album)).collect();
+
+            return Template::render(
+                "not_playing",
+                &NotPlayingContext {
+                    scrobble_count: &user_stats.scrobble_count,
+                    account_creation: &account_creation,
+                    albums,
+                    user: &user,
+                    key: &key,
+                },
+            );
         }
-        return Template::render(
-            "index",
-            &TemplateContext {
-                name: "None",
-                track: "None",
-                artist: "None",
-                album: "None",
-                album_art: "None",
-                user: &user,
-                key: &key,
-            },
-        );
     }
-    Template::render(
-        "index",
-        &TemplateContext {
-            name: "None",
-            track: "None",
-            artist: "None",
-            album: "None",
-            album_art: "None",
-            user: "None",
-            key: "None",
-        },
-    )
+
+    // If the user and key are not passed in through the URL, prompt user for them
+    Template::render("no_params", &ApiContext { user, key })
 }
 
 #[catch(404)]
--- src/tera/lastfm.rs
+++ src/tera/lastfm.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 use reqwest::Error;
 
 #[derive(Debug)]
@@ -8,14 +10,51 @@ pub struct Track {
     pub album_art: String,
 }
 
+#[derive(Debug)]
+pub struct UserStats {
+    pub scrobble_count: u32,
+    pub registered_time: i64,
+}
+
+#[derive(Debug)]
+pub struct Album {
+    pub name: String,
+    pub album_art: String,
+}
+
+#[allow(dead_code)]
+// Only QueryPeriod::SevenDay is used right now
+pub enum QueryPeriod {
+    Overall,
+    SevenDay,
+    OneMonth,
+    ThreeMonths,
+    SixMonths,
+    TwelveMonths,
+}
+
+impl fmt::Display for QueryPeriod {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            QueryPeriod::Overall => write!(f, "overall"),
+            QueryPeriod::SevenDay => write!(f, "7day"),
+            QueryPeriod::OneMonth => write!(f, "1month"),
+            QueryPeriod::ThreeMonths => write!(f, "3month"),
+            QueryPeriod::SixMonths => write!(f, "6month"),
+            QueryPeriod::TwelveMonths => write!(f, "12month"),
+        }
+    }
+}
+
 pub async fn get_current_playing(username: &str, api_key: &str) -> Result<Option<Track>, Error> {
     let request_url = format!(
         "https://ws.audioscrobbler.com/\
-                            2.0/?method=user.getrecenttracks\
+                            2.0/?method={method}\
                             &user={username}&api_key={api_key}\
-                            &limit=2&format=json",
+                            &format=json",
+        method = "user.getrecenttracks",
         username = username,
-        api_key = api_key
+        api_key = api_key,
     );
 
     let response = reqwest::get(&request_url).await?;
@@ -48,4 +87,70 @@ pub async fn get_current_playing(username: &str, api_key: &str) -> Result<Option
         artist: artist,
         album_art: art,
     }))
+}
+
+pub async fn get_user_stats(username: &str, api_key: &str) -> Result<UserStats, Error> {
+    let request_url = format!(
+        "https://ws.audioscrobbler.com/\
+                            2.0/?method={method}\
+                            &user={username}&api_key={api_key}\
+                            &format=json",
+        method = "user.getinfo",
+        username = username,
+        api_key = api_key,
+    );
+
+    let response = reqwest::get(&request_url).await?;
+    let json = response.json::<serde_json::Value>().await?;
+
+    let scrobble_count = json["user"]["playcount"]
+        .as_str()
+        .unwrap()
+        .parse::<u32>()
+        .unwrap();
+    let registered_time = json["user"]["registered"]["#text"]
+        .as_i64()
+        .unwrap();
+    println!("{}", format!("Scrobbles: {}", scrobble_count));
+
+    Ok(UserStats { scrobble_count, registered_time })
+}
+
+pub async fn get_top_albums(
+    username: &str,
+    api_key: &str,
+    period: QueryPeriod,
+    limit: &u8,
+) -> Result<Vec<Album>, Error> {
+    let request_url = format!(
+        "https://ws.audioscrobbler.com/\
+                            2.0/?method={method}\
+                            &user={username}&api_key={api_key}\
+                            &period={period}&limit={limit}\
+                            &format=json",
+        method = "user.gettopalbums",
+        username = username,
+        api_key = api_key,
+        period = period.to_string(),
+        limit = limit.to_string(),
+    );
+
+    let response = reqwest::get(&request_url).await?;
+    let json = response.json::<serde_json::Value>().await?;
+
+    let mut albums = Vec::new();
+    let json_albums = json["topalbums"]["album"].as_array().unwrap();
+
+    for album in json_albums {
+        albums.push(Album {
+            name: String::from(album["name"].as_str().unwrap()),
+            album_art: String::from(album["image"][3]["#text"].as_str().unwrap()),
+        });
+
+        if albums.len() >= *limit as usize {
+            break;
+        }
+    }
+
+    Ok(albums)
 }
